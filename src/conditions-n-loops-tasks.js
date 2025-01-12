a9b812477f20e3471c578830cb25eb34fe56fc7f
annalatyeva <anysha_s@mail.ru>
2025-01-12 21:46:13 +0500

    feat: finish from 7 to 9

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -244,8 +244,13 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  for (let i = 0, j = str.length - 1; i + 1 <= str.length / 2; i += 1, j -= 1) {
+    if (str[i] !== str[j]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -262,8 +267,13 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -281,8 +291,17 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let number = num;
+  if (number < 0) {
+    number = -number;
+  }
+  for (; number > 0; number = Math.floor(number / 10)) {
+    if (number % 10 === digit) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
